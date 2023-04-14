daa043448a2e71fd710bf4b561a269506f67e1ef
Yashgit2000 <yashgod16@gmail.com>
2023-04-14 14:45:35 +0530

    update

--- cicd-s3.tf
+++ cicd-s3.tf
@@ -6,3 +6,12 @@ resource "aws_s3_bucket_acl" "code-pipeline-artifacts1_acl" {
   bucket = aws_s3_bucket.code-pipeline-artifacts1.id
   acl    = "private"
 }
+
+resource "aws_s3_bucket" "code-pipeline-artifacts123" {
+  bucket = "code-pipeline-artifacts123"
+}
+
+resource "aws_s3_bucket_acl" "code-pipeline-artifacts123_acl" {
+  bucket = aws_s3_bucket.code-pipeline-artifacts123.id
+  acl    = "private"
+}
