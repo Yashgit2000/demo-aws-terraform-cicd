0ccb1c848b92aad9bff97cb27bf48a79f2ea7d20
Yashgit2000 <yashgod16@gmail.com>
2023-04-14 13:50:57 +0530

    update

--- variables.tf
+++ variables.tf
@@ -1,9 +1,7 @@
 variable dockerhub_credentials{
     type = string
-    default="arn:aws:secretsmanager:ap-south-1:875225952044:secret:codebuild/dockerhubb-qikhmd"
 }
 
 variable codestar_connector_credentials {
     type = string
-    default = "arn:aws:codestar-connections:ap-south-1:875225952044:connection/fce305e8-2b0b-4ebd-aa4c-7e3b36dc716f"
 }
